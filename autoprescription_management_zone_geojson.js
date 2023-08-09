4f33e393a4f19fd961df1836e52fff97c6441cb3
agronomofiorentini <66969646+agronomofiorentini@users.noreply.github.com>
2023-08-09 09:46:37 +0200

    Update autoprescription_management_zone_geojson.js

--- autoprescription_management_zone_geojson.js
+++ autoprescription_management_zone_geojson.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 const axios = require('axios');
 
+// set username & password
+
 const username = 'XXXXXXXXXXXXXXX';
 const password = 'XXXXXXXXXXXXXXX';
 
+// set api endpoint
+
 const apiEndpoint = 'https://www.api.automaticfarmsolutionwebapp.com/AFS/AutoPrescriptionMapGEOjson?AutoPrescription=False&NumberZone=4&userfertilizer=200&Strategy=highwherehigh';
+
+// set the path to the geojson to send as body of the post request
+
 const geojsonFilePath = 'county.geojson';
-const outputFilePath = 'result.geojson'; // Percorso di destinazione del nuovo file GeoJSON
+
+// set the path to the save the geojson that you will receve as response
+
+const outputFilePath = 'result.geojson';
 
 
 (async () => {
   try {
-    // Leggi il contenuto del file GeoJSON
+    // Read the geojson content
     const geojsonContent = await fs.promises.readFile(geojsonFilePath, 'utf8');
-    // Parsa il contenuto come JSON
+    // Parse the geojson content
     const geojsonObject = JSON.parse(geojsonContent);
 
-    // Opzioni per l'autenticazione basata su username e password
+    // Set option to do the authentication
     const authHeader = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
 
-    // Esegui la richiesta POST all'API con il body GeoJSON e l'autenticazione
+    // Sed the post request
     const response = await axios.post(apiEndpoint, geojsonObject, {
       headers: {
         'Content-Type': 'application/json',
@@ -28,7 +38,7 @@ const outputFilePath = 'result.geojson'; // Percorso di destinazione del nuovo f
     });
 
     const resultGeoJSON = response.data;
-    // Gestisci la risposta dell'API qui
+    // See the response of the api in the console
     console.log('Risposta dall\'API:', resultGeoJSON);
 
     await fs.promises.writeFile(outputFilePath, JSON.stringify(resultGeoJSON, null, 2), 'utf8');
